5148166c9501036f6c4fce5b4f72787905075907
Jennifer Reeves <jbreeves@gmail.com>
2023-02-08 22:24:44 -0500

    Add files via upload
    Python script to bulk change xml files to json

---
+++ bulk xml to json.py
@@ -0,0 +1,31 @@
+import os
+import xmltodict
+
+# Define the directory that contains the XML files
+xml_directory = '/path/to/xml/files'
+
+# Define the directory to write the JSON files to
+json_directory = '/path/to/json/files'
+
+# Loop over all files in the directory
+for filename in os.listdir(xml_directory):
+    # Check if the file is an XML file
+    if filename.endswith(".xml"):
+        # Construct the full path to the file
+        file_path = os.path.join(xml_directory, filename)
+
+        # Open the XML file
+        with open(file_path, "r") as file:
+            # Read the XML data from the file
+            xml_data = file.read()
+
+        # Convert XML to JSON
+        json_data = xmltodict.parse(xml_data)
+
+        # Construct the full path to the output JSON file
+        json_file = os.path.join(json_directory, os.path.splitext(filename)[0] + ".json")
+
+        # Save the JSON data to a file
+        with open(json_file, "w") as file:
+            file.write(str(json_data))
+
